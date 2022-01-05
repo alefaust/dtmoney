f467b3c8e4fc1c940bf6b1d0e5e9e906ec1234a5
Alexandre Faustino <alexandre.faustino@wkm.com.br>
2022-01-05 18:00:45 -0300

    [FASE-4] Implementando Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,12 @@
 import React,{useEffect} from 'react';
 
 import { Container } from './styles';
+import { api } from '../../services/api';
 
 export const TransactionsTable: React.FC = () => {
   useEffect(() => {
-    fecth('http://localhost:3000/api/transactions')
-    .then(response => response.json())
-    .then(data => console.log(data));
+    api.get('transactions')
+    .then(response => console.log(response.data));
   },[])
 
   return (
