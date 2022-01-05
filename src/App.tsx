dd60283bde9d61fc03c360fea6512f339f0b99c3
Alexandre Faustino <alexandre.faustino@wkm.com.br>
2022-01-05 18:44:27 -0300

    [FASE-5] Implementação do react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState}  from 'react';
+import Modal from 'react-modal';
 import {GlobalStyle} from './styles/global';
 import {Dashboard} from './components/Dashboard';
 import { Header } from './components/Header';
-export function App() {
+
+Modal.setAppElement('#root');
+
+export const App : React.FC = () =>{
+    const [isNewTransactionModalOpen,setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenAndCloseNewTransactionModalOpen () {
+    setIsNewTransactionModalOpen( old => !old);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenAndCloseNewTransactionModalOpen}/>
       <Dashboard />
+      <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleOpenAndCloseNewTransactionModalOpen}>
+          <h2>Cadastrar transação</h2>
+        </Modal>
       <GlobalStyle />
     </>
   );
